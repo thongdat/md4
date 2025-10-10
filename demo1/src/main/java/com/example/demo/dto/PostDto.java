16e46af7e94f8f8ffe8337ee81fd985393fa31a0
Your Name <you@example.com>
2025-10-10 19:18:24 +0700

    Nop bai

--- demo1/src/main/java/com/example/demo/dto/PostDto.java
+++ demo1/src/main/java/com/example/demo/dto/PostDto.java
@@ -1,14 +1,19 @@
 package com.example.demo.dto;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Data
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostDto {
