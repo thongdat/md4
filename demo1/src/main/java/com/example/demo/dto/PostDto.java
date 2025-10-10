f256263b0b7751404f9a2a541a814a5cec4dd0ef
Your Name <you@example.com>
2025-10-10 19:13:51 +0700

    Nop bai

--- demo1/src/main/java/com/example/demo/dto/PostDto.java
+++ demo1/src/main/java/com/example/demo/dto/PostDto.java
@@ -1,18 +1,30 @@
 package com.example.demo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
+import lombok.Setter;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostDto {
+
     private Integer id;
+
+    @NotBlank(message = "Tiêu đề không được để trống.")
     private String title;
+
+    @NotBlank(message = "Nội dung không được để trống.")
     private String content;
+
     private String summary;
+
     private LocalDateTime createdAt;
+
+    @NotNull(message = "Vui lòng chọn một danh mục.")
     private CategoryDto category;
 }
